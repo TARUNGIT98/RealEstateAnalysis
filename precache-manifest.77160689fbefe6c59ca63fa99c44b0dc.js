88552a89cab3f5f3600ab5cdd821e5a856022baf
Tarun Sirapurapu <tarunrhymes7@gmail.com>
2025-02-18 22:34:42 -0500

    Updates

---
+++ precache-manifest.77160689fbefe6c59ca63fa99c44b0dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458bc1a78de73728464dd5cdb89b502c",
+    "url": "/RealEstateAnalysis/index.html"
+  },
+  {
+    "revision": "84f89986a9e43b516678",
+    "url": "/RealEstateAnalysis/static/css/main.de54cc74.chunk.css"
+  },
+  {
+    "revision": "5a77b56fe485d12df9f0",
+    "url": "/RealEstateAnalysis/static/js/2.6d789d82.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/RealEstateAnalysis/static/js/2.6d789d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929371b014160c5fef78",
+    "url": "/RealEstateAnalysis/static/js/3.81704da5.chunk.js"
+  },
+  {
+    "revision": "84f89986a9e43b516678",
+    "url": "/RealEstateAnalysis/static/js/main.ef468ab8.chunk.js"
+  },
+  {
+    "revision": "19b916bb7c199e0d4aae",
+    "url": "/RealEstateAnalysis/static/js/runtime-main.4436114f.js"
+  }
+]);
