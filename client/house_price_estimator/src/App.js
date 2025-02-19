837290f8e98662d22debb7c8534270cd212b0028
Tarun Sirapurapu <tarunrhymes7@gmail.com>
2025-02-19 00:42:11 -0500

    Fixed backticks

--- client/house_price_estimator/src/App.js
+++ client/house_price_estimator/src/App.js
@@ -9,10 +9,10 @@ const HomePriceEstimator = () => {
   const [locations, setLocations] = useState([]);
   const [estimatedPrice, setEstimatedPrice] = useState(null);
 
-  const API_BASE_URL = "https://realestateanalysis.onrender.com/api"; // Backend Render URL
+  const API_BASE_URL = "https://realestateanalysis.onrender.com"; // Backend Render URL
 
   useEffect(() => {
-    fetch(`${API_BASE_URL}/get_location_names`)
+    fetch(`${API_BASE_URL}/api/get_location_names`)
       .then(response => response.json())
       .then(data => {
         if (data.locations) {
@@ -28,7 +28,7 @@ const HomePriceEstimator = () => {
       return;
     }
 
-    const response = await fetch(`${API_BASE_URL}/predict_home_price`, {
+    const response = await fetch(`${API_BASE_URL}/api/predict_home_price`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
